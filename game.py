b5ff6634fa27948471d4a0c6a083b7e8e2bbdb23
miheisv <misha.struko@mail.ru>
2024-07-18 15:17:38 +0300

    Commit 002 Созданы разделы: приветствие, меню, переход к игре, игра Разработаны прототипы разделов: игра

---
+++ game.py
@@ -0,0 +1,24 @@
+class Game:
+    def __init__(self, chat_id, stage, word):
+        self.chat_id = chat_id
+        self.stage = stage
+        self.word = word
+
+    def get_answer(self, text):
+        text = text.lower()
+        answer = ''
+        mask = list(self.word)
+        if len(self.word) != len(text):
+            return self.answer
+        for i in range(len(text)):
+            if text[i] == self.word[i]:
+                letter = text[i].upper()
+                answer += '__{letter}__'
+                mask.remove(text[i])
+            elif text[i] in mask:
+                letter = text[i].upper()
+                answer += letter
+            else:
+                letter = text[i].lower()
+                answer += letter
+        return answer
